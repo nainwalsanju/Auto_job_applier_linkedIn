891440028ef2eda4048b11885ba79f0a5113bf76
Sanjay Nainwal <37777780+nainwalsanju@users.noreply.github.com>
2026-01-29 22:27:49 +0530

    asdsad

--- config/recruiter_messaging.py
+++ config/recruiter_messaging.py
@@ -10,7 +10,7 @@ License:    GNU Affero General Public License
 GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
 
 
-
+  
 version:    26.01.18.22.50sdsd
 
 Contributor: Sanjay Nainwal (sanjaynainwal129@gmail.com) - Feature: Recruiter Messaging
